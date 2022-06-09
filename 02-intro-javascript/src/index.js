582a697573efabc27edcb9ef815a44ed63589332
Marco Tulio Ruiz <mruizq@unah.hn>
2022-06-09 07:46:14 -0600

    CH20 - Imports and Exports

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,18 @@
-console.log('CH19')
+import {heroes} from './data/heroes';
+
+// const getHeroById = (id) => {
+//     return heroes.find( (hero) => {
+//         if( hero.id === id ){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     });
+// }
+
+const getHeroById = (id) => heroes.find( (hero) => hero.id === id);
+console.log( getHeroById(2) );
+
+const getHeroByOwner = (owner) => heroes.filter( (hero) => hero.owner === owner);
+
+console.log( getHeroByOwner('DC') )
