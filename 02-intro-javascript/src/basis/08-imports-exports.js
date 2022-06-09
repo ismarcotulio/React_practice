8284ab409e381c0cc795a57b1026078ac37ac0f4
Marco Tulio Ruiz <mruizq@unah.hn>
2022-06-09 13:34:54 -0600

    CH21: Multiple exportation

---
+++ 02-intro-javascript/src/basis/08-imports-exports.js
@@ -0,0 +1,21 @@
+import heroes, {owners} from './data/heroes';
+
+
+console.log( owners );
+
+// const getHeroById = (id) => {
+//     return heroes.find( (hero) => {
+//         if( hero.id === id ){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     });
+// }
+
+const getHeroById = (id) => heroes.find( (hero) => hero.id === id);
+console.log( getHeroById(2) );
+
+const getHeroByOwner = (owner) => heroes.filter( (hero) => hero.owner === owner);
+
+console.log( getHeroByOwner('DC') )
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,18 +1,1 @@
-import {heroes} from './data/heroes';
-
-// const getHeroById = (id) => {
-//     return heroes.find( (hero) => {
-//         if( hero.id === id ){
-//             return true;
-//         }else{
-//             return false;
-//         }
-//     });
-// }
-
-const getHeroById = (id) => heroes.find( (hero) => hero.id === id);
-console.log( getHeroById(2) );
-
-const getHeroByOwner = (owner) => heroes.filter( (hero) => hero.owner === owner);
-
-console.log( getHeroByOwner('DC') )
+console.log("CH21")
