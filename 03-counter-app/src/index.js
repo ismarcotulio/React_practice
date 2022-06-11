d18009d5e896dd39ade40aca4e9dfe3226fd9d6c
Marco Tulio Ruiz <martulioruiz@gmail.com>
2022-06-11 17:26:55 -0600

    CH41 - CounterApp Component task

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import FirstApp from "./FirstApp";
+// import FirstApp from "./FirstApp";
+import CounterApp from "./CounterApp";
 
 import './index.css';
 
 const divRoot = document.querySelector( '#root' );
 
-ReactDOM.render( <FirstApp greeting='Hi, I am Goku' />, divRoot );
+ReactDOM.render( <CounterApp value={ 123 } />, divRoot );
