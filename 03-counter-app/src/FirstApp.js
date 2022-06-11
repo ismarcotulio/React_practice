6ac8514461d18f1168e0c47f32aa385cb07196e9
Marco Tulio Ruiz <martulioruiz@gmail.com>
2022-06-11 17:15:02 -0600

    CH40 - DefaultProps

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FirstApp = ( { greeting = 'Hello World' } ) => {
+const FirstApp = ( { greeting = 'Hello World', subtitle } ) => {
 
     return (
         <>
             <h1> { greeting } </h1>
-            <p>This is paragraph</p>
+            <p>{ subtitle }</p>
         </>
     )
 }
 
 FirstApp.propTypes = {
     greeting: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    subtitle: 'I am a subtitle'
 }
 
 export default FirstApp;
