0745cf698e2aef4892f6ce1338453de6d523f7ab
Marco Tulio Ruiz <martulioruiz@gmail.com>
2022-06-11 16:14:36 -0600

    CH36 - Fragment

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -1,7 +1,20 @@
-import React from 'react';
+// import React, { Fragment } from 'react';
 
 const FirstApp = () => {
-    return <h1>Hello World</h1>;
+    /*
+    return (
+        <Fragment>
+            <h1>Hello World</h1>
+            <p>This is paragraph</p>
+        </Fragment>
+    )
+    */
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>This is paragraph</p>
+        </>
+    )
 }
 
 export default FirstApp;
