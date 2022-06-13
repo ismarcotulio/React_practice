40062819c60ae42523952631945dff4b535f4b23
Marco Tulio Ruiz <martulioruiz@gmail.com>
2022-06-12 18:31:11 -0600

    CH53: Jest - expect and toBe

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,4 +1,14 @@
+describe('test in <DemoComponent />', () => { 
+    test('This test should not fail', () => {
 
-test('This test should not fail', () => {
-
-});
+        // 1. Arrange
+        const message1 = 'Hello World';
+    
+        // 2. Act
+        const message2 = message1.trim();
+    
+        // 3. Assert
+        expect( message1 ).toBe( message2 );
+    
+    });
+})
